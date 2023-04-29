78571e74f5ece7007c8c8b58d88666212a7298fa
lucas.gois <lucassampaiodegois1@gmail.com>
2023-04-29 18:19:59 +0000

    chore: add proto file configurarion

---
+++ configs/config.go
@@ -0,0 +1,45 @@
+package configs
+
+type conf struct {
+	DBDriver           string   `mapstructure:"DB_DRIVER"`
+	DBHost             string   `mapstructure:"DB_HOST"`
+	DBPort             string   `mapstructure:"DB_PORT"`
+	DBUser             string   `mapstructure:"DB_USER"`
+	DBPassword         string   `mapstructure:"DB_PASSWORD"`
+	DBName             string   `mapstructure:"DB_NAME"`
+	WebServerPort      string   `mapstructure:"WEB_SERVER_PORT"`
+	GRPCServerPort     string   `mapstructure:"GRPC_SERVER_PORT"`
+	InitialChatMessage string   `mapstructure:"INITIAL_CHAT_MESSAGE"`
+	OpenAIApiKey       string   `mapstructure:"OPENAI_API_KEY"`
+	Model              string   `mapstructure:"MODEL"`
+	ModelMaxTokens     int      `mapstructure:"MODEL_MAX_TOKENS"`
+	Temperature        float64  `mapstructure:"TEMPERATURE"`
+	TopP               float64  `mapstructure:"TOP_P"`
+	N                  int      `mapstructure:"N"`
+	Stop               []string `mapstructure:"STOP"`
+	MaxTokens          int      `mapstructure:"MAX_TOKENS"`
+	AuthToken          string   `mapstructure:"AUTH_TOKEN"`
+}
+
+func LoadConfig(path string) (*conf, error) {
+	return &conf{
+		DBDriver:           "mysql",
+		DBHost:             "mysql",
+		DBPort:             "3306",
+		DBUser:             "root",
+		DBPassword:         "root",
+		DBName:             "jarvis",
+		WebServerPort:      "8080",
+		GRPCServerPort:     "50051",
+		InitialChatMessage: "Seu nome é Jarvis. Você é a inteligência artificial de Lucas Gois. Você da suporte a programadores e arquitetos de software",
+		OpenAIApiKey:       "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",
+		Model:              "gpt-3.5-turbo",
+		ModelMaxTokens:     4096,
+		Temperature:        0.2,
+		TopP:               0.2,
+		N:                  1,
+		Stop:               []string{"super-end"},
+		MaxTokens:          300,
+		AuthToken:          "123456",
+	}, nil
+}
